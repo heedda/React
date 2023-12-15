55e2bb9a99ea000ff43ae1a1db060877b5cd009a
heedda <heedda.kollim@gmail.com>
2023-12-15 20:27:52 +0900

    react 공부

--- react-test/src/EventPratice.js
+++ react-test/src/EventPratice.js
@@ -2,27 +2,29 @@ import React, { Component } from "react";
 
 class EventPratice extends Component {
   state = {
+    username: "",
     message: "",
   };
 
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
-  handleClick() {
-    alert(this.state.message);
+  handleClick = () => {
+    alert(this.state.username + ": " + this.state.message);
     this.setState({
+      username: "",
       message: "",
     });
-  }
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
+  };
 
   render() {
     return (
@@ -30,11 +32,19 @@ class EventPratice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+          autoFocus
+        />
+        <input
+          type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
-          autoFocus
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
